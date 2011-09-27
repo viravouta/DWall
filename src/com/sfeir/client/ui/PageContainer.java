ad54a7653a142ea934dc0a4c9b78dfa54effffe8
Alexandre Viravout <viravout.a@sfeir.com>
2011-09-27 14:58:28 +0200

    Add image animation on click.

--- src/com/sfeir/client/ui/PageContainer.java
+++ src/com/sfeir/client/ui/PageContainer.java
@@ -1,11 +1,16 @@
 package com.sfeir.client.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Grid;
-import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -13,7 +18,7 @@ import com.google.gwt.user.client.ui.Widget;
 public class PageContainer extends Composite {
 
 	@UiField
-	HTMLPanel pageContainer;
+	AbsolutePanel pageContainer;
 	@UiField
 	SimplePanel gridContainer;
 	@UiField
@@ -24,22 +29,78 @@ public class PageContainer extends Composite {
 	interface PageContainerUiBinder extends UiBinder<Widget, PageContainer> {}
 	private static PageContainerUiBinder uiBinder = GWT.create(PageContainerUiBinder.class);
 
+	/**
+	 * Full screen state.
+	 */
+	private boolean fullScreen;
+	
+	private final List<Image> images;
+	
 	public PageContainer() {
 		this.initWidget(uiBinder.createAndBindUi(this));
 		
-		this.initWidgetCss();
+		this.images = new ArrayList<Image>();
+		this.images.add(image1);
+		this.images.add(image2);
+		this.images.add(image3);
+		this.images.add(image4);
+		
+		this.initWidgets();
+		this.initWidgetsCss();
+		this.initWidgetsHandler();
 	}
 
-	private void initWidgetCss() {
+	private void initWidgets() {
+		this.fullScreen = false;
+	}
+	
+	private void initWidgetsCss() {
 		pageContainer.addStyleName("pageContainer");
 		
 		gridContainer.addStyleName("gridContainer");
 		grid.addStyleName("grid");
 		
-		image1.addStyleName("image");
-		image2.addStyleName("image");
-		image3.addStyleName("image");
-		image4.addStyleName("image");
+		for(Image image : this.images) {			
+			image.addStyleName("image");
+		}
+	}
+	
+	private void initWidgetsHandler() {
+		ClickHandler onClickImage = new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				final Image imageClicked = (Image)event.getSource();
+				
+				
+				if(PageContainer.this.fullScreen) {
+					for(Image image : PageContainer.this.images) {
+						image.setVisible(true);
+					}
+					
+					imageClicked.addStyleName("image");
+					imageClicked.removeStyleName("imageClicked");
+					
+					PageContainer.this.fullScreen = false;
+				} else {			
+					for(Image image : PageContainer.this.images) {
+						image.setVisible(false);
+					}
+					
+					imageClicked.setVisible(true);
+					imageClicked.removeStyleName("image");
+					imageClicked.addStyleName("imageClicked");
+					
+					PageContainer.this.fullScreen = true;
+				}
+				
+			}
+		};
+		
+		image1.addClickHandler(onClickImage);
+		image2.addClickHandler(onClickImage);
+		image3.addClickHandler(onClickImage);
+		image4.addClickHandler(onClickImage);
 	}
 	
 }
